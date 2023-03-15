50502b8b1347f4e306cca1b1d60b07547cfbc037
dedani4 <dedani4@gmail.com>
2023-03-15 20:35:30 +0300

    init

---
+++ Python_Seminar_101/Seminar_1/main2.py
@@ -0,0 +1,20 @@
+# n = 1.89
+# print(n)
+# n = 'fg\'gf'
+# print(n)
+# print(type(n))
+# print('input fuckin string')
+
+a1 = input('please\n')
+
+a = 1
+b = 5.5
+c = 'sdfgh'
+
+print(f"{a1}-{b}-{c}")
+print(type(b))
+b = int(b)
+print(type(b))
+
+
+print("{}-{}-{}".format(a, c, b))
